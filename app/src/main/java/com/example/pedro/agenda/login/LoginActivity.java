77ef9ca17dd41ab345421006560286ecfc7ebaec
João Pedro Silva <jsangregorio@hotmail.com>
2017-10-18 14:58:05 -0200

    MVP no Login

---
+++ app/src/main/java/com/example/pedro/agenda/login/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.example.pedro.agenda.login;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pedro.agenda.CadastroDeContatoActivity;
+import com.example.pedro.agenda.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
+
+public class LoginActivity extends AppCompatActivity implements LoginView {
+
+    @BindView(R.id.layout_login)
+    TextInputLayout LayoutLogin;
+    @BindView(R.id.layout_password)
+    TextInputLayout LayoutPassword;
+    @BindView(R.id.text_edit_login)
+    TextInputEditText TextEditLogin;
+    @BindView(R.id.text_edit_password)
+    TextInputEditText TextEditPassword;
+    LoginPresenter loginPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        loginPresenter = new LoginPresenter(LoginActivity.this);
+
+
+    }
+
+    @OnTextChanged(R.id.text_edit_login)
+    public void validaLogin() {
+        LayoutLogin.setErrorEnabled(false);
+        LayoutLogin.setError("");
+    }
+
+    @OnTextChanged(R.id.text_edit_password)
+    public void validaSenha() {
+        LayoutLogin.setErrorEnabled(false);
+        LayoutLogin.setError("");
+    }
+
+    @OnClick(R.id.botao_login)
+    public void FazLogin() {
+        loginPresenter.login(TextEditLogin.getText().toString(), TextEditPassword.getText().toString());
+    }
+
+    @Override
+    public void SetErroUsuario() {
+        LayoutLogin.setErrorEnabled(true);
+        LayoutLogin.setError("LoginActivity incorreto!");
+    }
+
+    @Override
+    public void SetErroSenha() {
+        LayoutPassword.setErrorEnabled(true);
+        LayoutPassword.setError("Senha incorreta!");
+    }
+
+    @Override
+    public void LoginSucesso() {
+        Intent abrirListaDeContatos = new Intent(LoginActivity.this, CadastroDeContatoActivity.class);
+        startActivity(abrirListaDeContatos);
+    }
+}
